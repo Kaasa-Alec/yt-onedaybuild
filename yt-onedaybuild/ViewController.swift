6dedbec16e48f36a8497d7b7b5b6b89c73eac6b0
Kaasa-Alec <kaa15001@byui.edu>
2020-07-03 12:25:30 -0700

    initial xcode project creation

---
+++ yt-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yt-onedaybuild
+//
+//  Created by Alec Kaasa on 7/3/20.
+//  Copyright © 2020 Alec Kaasa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
